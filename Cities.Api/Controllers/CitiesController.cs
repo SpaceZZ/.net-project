3b00f2970739f28070f35f1fdbb03bcfd5c72c1b
SpaceZZ <kapala.adam@gmail.com>
2018-11-29 17:33:09 +0100

    Cities.api initialized

---
+++ Cities.Api/Controllers/CitiesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cities.Api.Controllers
+{
+    public class CitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cities.Api/Models/CitiesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cities.Api.Models
+{
+    public class CitiesDTO
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set ;}
+    }
+}
